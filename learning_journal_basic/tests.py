14a28798a814e3cc100d7316e07a2826e565a312
Ford Fowler <fordjfowler@gmail.com>
2016-12-20 19:09:46 -0800

    setup some templates using jinja

--- learning_journal_basic/tests.py
+++ learning_journal_basic/tests.py
@@ -1,29 +1,37 @@
-import unittest
-
+"""Testing module."""
+import pytest
 from pyramid import testing
 
 
-class ViewTests(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'learning_journal_basic')
+@pytest.fixture
+def req():
+    reque = testing.DummyRequest()
+    return reque
 
 
-class FunctionalTests(unittest.TestCase):
-    def setUp(self):
-        from learning_journal_basic import main
-        app = main({})
-        from webtest import TestApp
-        self.testapp = TestApp(app)
+def test_home_view_renders_home_data(req):
+    """My home page view returns dictionary."""
+    from .views import home
+    response = home(req).replace(' ', '')
+    assert 'bag' in response
 
-    def test_root(self):
-        res = self.testapp.get('/', status=200)
-        self.assertTrue(b'Pyramid' in res.body)
+
+def test_home_has_iterable(req):
+    from .views import home
+    response = home(req)
+    assert hasattr(response['bag'], '__iter__')
+
+
+@pytest.fixture
+def testapp():
+    from webtest import TestApp
+    from learning_journal_basic import main
+    app = main({})
+    return TestApp(app)
+
+
+def test_home_has_list(testapp):
+    response = testapp.get('/', status=200)
+    inner_html = response.html
+    import pdb;r
+    assert len(inner_html.find_all('li')) == 3
--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,37 +1,46 @@
-from pyramid.response import Response
+from pyramid.view import view_config
 import os
 
 
 THIS_DIR = os.path.dirname(__file__)
 
 
+@view_config(route_name="home", renderer="templates/list.jinja2")
 def home(request):
     """Home view handler."""
-    file_contents = open(os.path.join(THIS_DIR, 'templates/index.html')).read()
-    return Response(file_contents)
+    # file_data = open(os.path.join(THIS_DIR, 'templates/index.html')).read()
+    stuff = [
+        'orange',
+        'pen',
+        'knife',
+    ]
+    return {"bag": stuff}
 
 
+@view_config(route_name="detail", renderer="string")
 def detail(request):
     """Detail view handler."""
-    file_contents = open(os.path.join(THIS_DIR, 'data/detail.html')).read()
-    return Response(file_contents)
+    file_data = open(os.path.join(THIS_DIR, 'data/detail.html')).read()
+    return file_data
 
 
+@view_config(route_name="update", renderer="string")
 def update(request):
     """Update/edit view handler."""
-    file_contents = open(os.path.join(THIS_DIR, 'templates/update.html')).read()
-    return Response(file_contents)
+    file_data = open(os.path.join(THIS_DIR, 'templates/update.html')).read()
+    return file_data
 
 
+@view_config(route_name="create", renderer="string")
 def create(request):
     """Create new view handler."""
-    file_contents = open(os.path.join(THIS_DIR, 'templates/create.html')).read()
-    return Response(file_contents)
+    file_data = open(os.path.join(THIS_DIR, 'templates/create.html')).read()
+    return file_data
 
 
-def includeme(config):
-    """Pyramid view configuration."""
-    config.add_view(home, route_name="home")
-    config.add_view(detail, route_name="detail")
-    config.add_view(update, route_name="update")
-    config.add_view(create, route_name="create")
+# def includeme(config):
+#     """Pyramid view configuration."""
+#     config.add_view(home, route_name="home")
+#     config.add_view(detail, route_name="detail")
+#     config.add_view(update, route_name="update")
+#     config.add_view(create, route_name="create")
